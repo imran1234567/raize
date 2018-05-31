1ce02360f5b9213381f3d0983ef88c4d6d68ebc0
imran1234567 <imran@kgec.edu.in>
2018-05-31 17:56:53 +0530

    BUGS

--- memberships/models.py
+++ memberships/models.py
@@ -38,5 +38,5 @@ class Membership(models.Model):
     membership_card = models.CharField(max_length=20, choices=MEMBERSHIP_CARD, default=SILVER)
 
 
-    def __str__(self):
+     def __str__(self):
         return str(self.name)
